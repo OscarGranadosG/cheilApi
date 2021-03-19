9aff18cf5f7c1723a20439a1f591b5a6e066d155
Oscar Granados <ojgranadosg@gmail.com>
2021-03-18 21:55:04 -0500

    First commit and setup database project

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Cheil Api</title>
+    </head>
+    <body class="antialiased">
+        <h1>Cheil Api, Laravel </h1>
+    </body>
+</html>
